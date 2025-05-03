dd163342956ea977ca0ffe3ae0932c5647b7808c
Saykat <mrsaykatm4@gmail.com>
2025-05-03 20:43:11 +0600

    5 Longest Palindrome Solve With Expand Around Center

--- src/string_problems/Longest_Palindromic_5_DP.java
+++ src/string_problems/Longest_Palindromic_5_DP.java
@@ -1,4 +1,4 @@
-public String longestPalindrome(String str) {
+/*public String longestPalindrome(String str) {
     int N = str.length();
     int palindrome_begins_at = 0;
     int palindrome_length = 1;
@@ -31,7 +31,33 @@ public String longestPalindrome(String str) {
     }
 
     return str.substring(palindrome_begins_at, palindrome_begins_at + palindrome_length);
+}*/
+
+public String longestPalindrome(String s) {
+    if (s == null || s.isEmpty()) return "";
+    int start = 0, end = 0;
+    for (int i = 0; i < s.length(); i++) {
+        int len1 = expandAroundCenter(s, i, i);
+        int len2 = expandAroundCenter(s, i, i + 1);
+        int len = Math.max(len1, len2);
+        if (len > end - start) {
+            start = i - (len - 1) / 2;
+            end = i + len / 2;
+        }
+    }
+    return s.substring(start, end + 1);
 }
+
+private int expandAroundCenter(String s, int left, int right) {
+    int L = left, R = right;
+    while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+        L--;
+        R++;
+    }
+    return R - L - 1;
+}
+
+
 public void main() {
     System.out.println(longestPalindrome("anagram"));
 }
